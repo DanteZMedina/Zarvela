e070e8c3fbadf018d0255ee9fd9c7f86e4d7cd80
Dante Medina <dantemx5@gmail.com>
2025-11-20 22:50:06 -0600

    Mejorando el menú de hamburguesa

--- js/main.js
+++ js/main.js
@@ -87,3 +87,16 @@ document.querySelectorAll('a.nav-link[href^="#"]').forEach((link) => {
     }
   });
 });
+
+
+/* ==== navbar ======== */ 
+// Navbar efecto scroll
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
